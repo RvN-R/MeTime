a8f230151931f1181c2c5ff59e53148b78d4a790
Atinos31 <atinosandra2014@gmail.com>
2022-01-28 20:10:07 +0000

    base template, signup,signin,homepage added

--- home/views.py
+++ home/views.py
@@ -6,3 +6,14 @@ from django.shortcuts import render
 def index(request):
     """a view to render homepage"""
     return render(request, "home/index.html")
+
+
+def resources(request):
+    """a view to render resources page"""
+    return render(request, "home/resources.html")
+
+
+
+def about(request):
+    """a view to render aboutpage"""
+    return render(request, "home/about.html")
