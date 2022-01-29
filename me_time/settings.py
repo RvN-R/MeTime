6d3c47327782331acfc58d68fcb97abca8dcabf8
RvN-R <rvannairn@googlemail.com>
2022-01-29 22:25:43 +0000

    WSGI Possible Bug Fix

--- me_time/settings.py
+++ me_time/settings.py
@@ -107,7 +107,7 @@ ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
-WSGI_APPLICATION = 'me_time.wsgi.application'
+WSGI_APPLICATION = 'metimeminds.wsgi.application'
 
 
 # Database
