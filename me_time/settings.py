a8e07e8329affbc031ae749fbd617dd18b969c3f
Atinos31 <atinosandra2014@gmail.com>
2022-01-29 11:48:48 +0000

    removed secret ket and set debug in settings

--- me_time/settings.py
+++ me_time/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dt-tpw+2%p7jq=+69sfirs2$10=3eh93njskgjhw(rw^^mo=7$'
+SECRET_KEY = os.get.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['me-time4.herokuapp.com', 'localhost']
 
