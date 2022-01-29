c63dde39b5a934fbe52cac50ef78e129534c73bd
RvN-R <rvannairn@googlemail.com>
2022-01-29 21:50:03 +0000

    Heroku 103

--- me_time/settings.py
+++ me_time/settings.py
@@ -18,7 +18,7 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for production!
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 
