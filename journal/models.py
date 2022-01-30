788576805b2a2c7f4f3c4665484a459d7a9261a8
RvN-R <rvannairn@googlemail.com>
2022-01-30 10:17:24 +0000

    journal model, admin and STATIC_ROOT added

--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Journal(models.Model):
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, null=False, blank=False)
+    comment = models.TextField(null=False, blank=False)
+    created = models.DateTimeField(auto_now=True)
+
+def __str__(self):
+    return self.title
--- me_time/settings.py
+++ me_time/settings.py
@@ -169,6 +169,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
